aa7f9b56bf0c5881c3ab5af9f8bd776a89e7ddeb
Arun Kumar <just.arunn@gmail.com>
2023-06-21 11:07:15 +0530

    logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import Logo from "../assets/logo.svg";
 const Header = () => {
   return (
     <header className="header">
       <nav className="nav-bar">
         <div className="logo">
-          <h2>Arun</h2>
+          <h2><img src={Logo} alt="" width={"6%"}/>Arun</h2>
         </div>
         <ul>
           <li>
